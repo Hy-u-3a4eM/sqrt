d549ae656e62f5b856c4408c88c9650d32e3769f
Hy-u-3a4eM <96656444+Hy-u-3a4eM@users.noreply.github.com>
2021-12-25 18:33:53 +0300

    Добавьте файлы проекта.

---
+++ sqrt/main.cpp
@@ -0,0 +1,39 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int sq_rt;
+    int uend;
+    do
+    {
+        cout << "Введи то число, из которого надо найти квадратный корень:" << endl;
+        cin >> sq_rt;
+        /*if (!cin)
+        {
+            cout << "Input Error!" << endl;
+            cin.clear();
+            fflush(stdin);
+        }
+        */
+        /*if (typeid(string) == typeid(sq_rt))
+        {
+            cout << "Не верен ввод" << endl;
+        }
+        */
+        if (sq_rt < 0)
+        {
+            cout << "Не верен ввод" << endl;
+        }
+        else
+        {
+            cout << "Ваш ответ: " << sqrt(sq_rt) << endl;
+        }
+        cout << "Вы хотите продолжить использовать программу (0 -- нет, закончить выполнение программы)?" << endl; //1 -- да, продолжить выполнение программы
+        cin >> uend;
+        system("CLS");
+    } while (uend != 0);
+    //system("pause");
+    keep_window_open();
+    return 0;
+}
